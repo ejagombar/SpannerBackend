161b23eeb2cb2e298067210476fc2608da2933fe
Edward Agombar <77460324+ejagombar@users.noreply.github.com>
2023-08-23 21:46:48 +0100

    create basic api

--- cmd/spanner/main.go
+++ cmd/spanner/main.go
@@ -1,7 +1,61 @@
-package spanner
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/ejagombar/SpannerBackend/pkg/shutdown"
+	"github.com/gofiber/fiber/v2"
+	"os"
+)
 
 func main() {
-	fmt.Println("Test")
+
+	// setup exit code for graceful shutdown
+	var exitCode int
+	defer func() {
+		os.Exit(exitCode)
+	}()
+
+	cleanup, err := run()
+
+	defer cleanup()
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		exitCode = 1
+		return
+	}
+
+	// ensure the server is shutdown gracefully & app runs
+	shutdown.Gracefully()
+}
+
+func run() (func(), error) {
+	app, err := buildServer()
+	if err != nil {
+		return nil, err
+	}
+
+	// start the server
+	go func() {
+		app.Listen("0.0.0.0:8080")
+	}()
+
+	// return a function to close the server and database
+	return func() {
+		app.Shutdown()
+	}, nil
+
+}
+
+func buildServer() (*fiber.App, error) {
+	// init the storage
+
+	// create the fiber app
+	app := fiber.New()
+
+	// add health check
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.SendString("Healthy!")
+	})
+
+	return app, nil
 }
